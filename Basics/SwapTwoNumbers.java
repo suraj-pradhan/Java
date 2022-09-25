55da4e102d2c7a57451bf20ddd1aa1babecf6300
Suraj-Pradhan <pradhansuraj423@gmail.com>
2022-09-25 17:58:22 +0530

    program to swap two numbers

---
+++ Basics/SwapTwoNumbers.java
@@ -0,0 +1,15 @@
+class SwapTwoNumbers {
+
+
+    public static void main(String [] args)
+    
+     {  int num1 = 10, num2 = 20;
+        int temp = num1;               // Aam Jindagi 
+        num1 = num2;                   // Aam Jindagi
+        num2 = temp;                   // Aam Jindagi
+
+        System.out.println(num1 + "\n"+ num2); // Aam jindagi 
+        // System.out.println("Number 1 is " + num2 + "\n" + "Number 2 is " + num1); // Mentos Jinadagi 
+     }
+}
+
