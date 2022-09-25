81d202769a65d25faa89dac06b072e5759159503
Suraj-Pradhan <pradhansuraj423@gmail.com>
2022-09-25 18:23:18 +0530

    Checking

--- Basics/SwapTwoNumbers.java
+++ Basics/SwapTwoNumbers.java
@@ -6,7 +6,7 @@ class SwapTwoNumbers {
      {  int num1 = 10, num2 = 20;
         int temp = num1;               // Aam Jindagi 
         num1 = num2;                   // Aam Jindagi
-        num2 = temp;                   // Aam Jindagi
+        num2 = temp;                   // Aam Jindag
 
         System.out.println(num1 + "\n"+ num2); // Aam jindagi 
         // System.out.println("Number 1 is " + num2 + "\n" + "Number 2 is " + num1); // Mentos Jinadagi 
